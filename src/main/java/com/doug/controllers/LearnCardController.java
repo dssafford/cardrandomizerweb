eab77017ec3eb9c11e0e3b4bcb7e9d3fd36d62ef
Doug Safford <doug.safford@yahoo.com>
2017-01-02 15:56:25 -0600

    nextOneLearnMasterCards works on next link, keeping card visible if already visible

--- src/main/java/com/doug/controllers/LearnCardController.java
+++ src/main/java/com/doug/controllers/LearnCardController.java
@@ -2,15 +2,13 @@ package com.doug.controllers;
 
 import com.doug.domain.Card;
 import com.doug.domain.CardInfo;
-import com.doug.domain.DeckAnswer;
-import com.doug.domain.Score;
 import com.doug.services.CardService;
-import com.doug.services.Helpers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
@@ -50,13 +48,24 @@ public class LearnCardController {
     @RequestMapping(value = "/nextOneLearnRandomCards", method = RequestMethod.GET)
     public String getNextRandomLearningCard(Model model) {
 
+
         model.addAttribute(getNextRandomLearningCard(counter));
+        model.addAttribute("counter", counter);
+        return "learning/nextOneLearnRandomCards";
+    }
+
+    @RequestMapping(value = "/previousOneLearnRandomCards", method = RequestMethod.GET)
+    public String getPreviousRandomLearningCard(Model model) {
+
+        model.addAttribute(getPreviousRandomLearningCard(counter));
         model.addAttribute("counter", counter);
         return "learning/nextOneLearnRandomCards";
     }
 
     @RequestMapping(value = "/nextOneLearnRandomCards", method = RequestMethod.POST)
     public String processRandomLearningAnswer(Model model, CardInfo cardInfo) {
+
+
 
         model.addAttribute(getNextRandomLearningCard(counter));
         model.addAttribute("counter", counter);
@@ -65,68 +74,140 @@ public class LearnCardController {
     }
 
     @RequestMapping(value = "/nextOneLearnMasterCards", method = RequestMethod.GET)
-    public String getNextMasterLearningCard(Model model) {
+    public String getNextMasterLearningCard(@RequestParam(value="cardVisible") String myCard, Model model) {
 
         model.addAttribute(getNextMasterLearningCard(counter));
         model.addAttribute("counter", counter);
+
+        if(myCard.equals("true")) {
+            model.addAttribute("cardVisible", "true");
+        } else {
+            model.addAttribute("cardVisible", "false");
+        }
+
+        //model.addAttribute("cardVisible", "false");
         return "learning/nextOneLearnMasterCards";
     }
 
+
+
+
+    @RequestMapping(value = "/previousOneLearnMasterCards", method = RequestMethod.GET)
+    public String getPreviousMasterLearningCard(Model model) {
+
+        model.addAttribute(getPreviousMasterLearningCard(counter));
+        model.addAttribute("counter", counter);
+
+
+
+
+        return "learning/nextOneLearnMasterCards";
+    }
     @RequestMapping(value = "/nextOneLearnMasterCards", method = RequestMethod.POST)
     public String processMasterLearningAnswer(Model model, CardInfo cardInfo) {
 
         model.addAttribute(getNextMasterLearningCard(counter));
         model.addAttribute("counter", counter);
 
+
+
         return "learning/nextOneLearnMasterCards";
     }
+
     protected String makeCardString(String cardName){
 
-        return cardName+".png";
+        if(cardName.indexOf(".png") == -1) {
+            return cardName + ".png";
+        }
 
+        return cardName;
     }
 
     private Card makeCard(String cardName) {
-        Card card = new Card();
-        card.setCardName(cardName + ".png");
 
-        return card;
+        if(cardName.indexOf(".png") == 0) {
+            Card card = new Card();
+            card.setCardName(cardName + ".png");
+            return card;
+        }
+
+        return null;
     }
 
     private CardInfo getNextRandomLearningCard(Integer id) {
-        CardInfo myCard = cachedRandomLearningCards.get(id);
 
-        myCard.setCardName(makeCardString(myCard.getCardName()));
 
-        this.counter=id+1;
+        if(id != 51) {
 
-        return myCard;
+            CardInfo myCard = cachedRandomLearningCards.get(id);
+
+            myCard.setCardName(makeCardString(myCard.getCardName()));
+
+            this.counter = id + 1;
+
+            return myCard;
+        }
+
+        return null;
+    }
+
+    private CardInfo getPreviousRandomLearningCard(Integer id) {
+
+
+        if(id != 0) {
+            CardInfo myCard = cachedRandomLearningCards.get(id);
+
+            myCard.setCardName(makeCardString(myCard.getCardName()));
+
+            this.counter = id - 1;
+            return myCard;
+        }
+
+        return null;
     }
 
     private CardInfo getNextMasterLearningCard(Integer id) {
-        CardInfo myCard = cachedRandomLearningCards.get(id);
 
-        myCard.setCardName(makeCardString(myCard.getCardName()));
+        if(id != 51) {
+            CardInfo myCard = learningCards.get(id);
 
-        this.counter=id+1;
+            myCard.setCardName(makeCardString(myCard.getCardName()));
 
-        return myCard;
+            this.counter = id + 1;
+
+            return myCard;
+        }
+        return null;
     }
 
-    @RequestMapping(value="/getRandomLearningList")
-    public String getRandomLearningList(Model model) {
+    private CardInfo getPreviousMasterLearningCard(Integer id) {
 
-        learningCards = CreateRandomLearningDeck();
+        if(id != -1) {
+            CardInfo myCard = learningCards.get(id);
 
+            myCard.setCardName(makeCardString(myCard.getCardName()));
 
+            this.counter = id - 1;
 
-        model.addAttribute("cards", learningCards);
-
-        return "learning/masterCardLearningRandomList";
-
+            return myCard;
+        }
+        return null;
     }
 
-    @RequestMapping(value="/learningMasterList", method = RequestMethod.GET)
+//    @RequestMapping(value="/getRandomLearningList")
+//    public String getRandomLearningList(Model model) {
+//
+//        learningCards = CreateRandomLearningDeck();
+//
+//
+//
+//        model.addAttribute("cards", learningCards);
+//
+//        return "learning/masterCardLearningRandomList";
+//
+//    }
+
+    @RequestMapping(value="/masterCardLearningMasterList", method = RequestMethod.GET)
     public String getMasterLearningList(Model model) {
 
         learningCards = cardService.createCardLearningMasterList();
@@ -135,7 +216,7 @@ public class LearnCardController {
 
         model.addAttribute("cards", learningCards);
 
-        return "learning/masterCardLearningList";
+        return "learning/masterCardLearningMasterList";
 
     }
 
